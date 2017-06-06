6210bc9f776a1542325017c35c9b6fe63c8d928e
Louis Viallon <viallo_l@etna-alternance.net>
2017-06-06 17:34:05 +0200

    Added cycle struct

---
+++ corewar/includes/cycle.h
@@ -0,0 +1,21 @@
+/*
+** cycle.h for CoreWar in /home/louis/CoreWar/corewar/includes
+**
+** Made by VIALLON Louis
+** Login   <viallo_l@etna-alternance.net>
+**
+** Started on  Tue Jun  6 16:27:50 2017 VIALLON Louis
+** Last update Tue Jun  6 16:28:53 2017 VIALLON Louis
+*/
+
+#ifndef _CYCLE__H_
+#define _CYCLE__H_
+
+typedef struct	s_cycle
+{
+    int	nbr;
+    int	live_calls;
+    int	die;
+}		t_cycle;
+
+#endif
--- corewar/src/main.c
+++ corewar/src/main.c
@@ -8,12 +8,37 @@
 ** Last update Mon Jun  5 13:28:53 2017 MARZI Nicolas
 */
 
+#include <libzvbi.h>
+#include <malloc.h>
 #include "libmy.h"
-#include "op.h"
-#include "parser.h"
+#include "../../sujet/op.h"
+#include "../includes/cycle.h"
+
+static t_cycle	*cycle_init()
+{
+    t_cycle	*cycle;
+
+    if ((cycle = (t_cycle*)malloc(sizeof(t_cycle))) != NULL)
+    {
+        cycle->nbr = 1;
+        cycle->live_calls = NBR_LIVE;
+        cycle->die = CYCLE_TO_DIE;
+    }
+    return (cycle);
+}
+
+int launch_corewar()
+{
+    if (cycle_init())
+        return (0);
+    return (-1);
+}
 
 int main(int argc, char **argv)
 {
-    parser(argc, argv);
-    return (0);
+    if (argc > 1 && argv != NULL)
+        return (launch_corewar());
+    else
+        my_putstr("corewar: At least one argument required\n");
+    return (-1);
 }
