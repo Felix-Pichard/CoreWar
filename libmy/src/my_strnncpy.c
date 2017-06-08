0c722f06eb694be62b6e1e6acf7bce628c1e37ed
Felix Pichard <felix.pichard@gmail.com>
2017-06-08 09:22:50 +0200

    Correct the norme

--- libmy/src/my_strnncpy.c
+++ libmy/src/my_strnncpy.c
@@ -15,10 +15,10 @@ char *my_strnncpy(char *src, int i, int size)
     char* str;
     int count;
 
-    if((str = malloc(sizeof(char)*size)) == NULL)
+    if((str = malloc(sizeof(char) * size)) == NULL)
         my_error(5);
     for (count = 0; src[i] != '\0' && count < size; ++i, ++count)
         str[count] = src[i];
 
-    return str;
+    return (str);
  }
