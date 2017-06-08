3cf59850f445a4599e63cced013101cb7cd32d89
Felix Pichard <felix.pichard@gmail.com>
2017-06-08 09:21:15 +0200

    Add headers

--- libmy/src/my_strnncpy.c
+++ libmy/src/my_strnncpy.c
@@ -1,3 +1,15 @@
+/*
+** main.c for COREWAR in /media/maniarr/Workspace/etna/CoreWar/asm/src
+**
+** Made by MARZI Nicolas
+** Login   <marzi_n@etna-alternance.net>
+**
+** Started on  Mon Jun  5 13:27:50 2017 MARZI Nicolas
+** Last update Mon Jun  5 13:28:53 2017 MARZI Nicolas
+*/
+
+#include <stdlib.h>
+
 char *my_strnncpy(char *src, int i, int size)
  {
     char* str;
@@ -10,4 +22,3 @@ char *my_strnncpy(char *src, int i, int size)
 
     return str;
  }
- 
