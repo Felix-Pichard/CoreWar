ba6bfcf66a65fde6e553f56c3e97f490fadc63e0
Felix Pichard <felix.pichard@gmail.com>
2017-06-08 15:11:14 +0200

    Add the structures needed for the virtual machine.

---
+++ corewar/includes/game.h
@@ -0,0 +1,39 @@
+#ifndef _GAME__H_
+#define _GAME__H_
+
+#include "op.h"
+
+typedef struct cursor_s
+{
+    int registers[REG_NUMBER + 1];
+    int position;
+    int cycles_left;
+} cursor_t;
+
+typedef struct program_s
+{
+    int id;
+    int alive;
+    char *name;
+} program_t;
+
+typedef struct game_s
+{
+    int         nb_player;
+    int         max_cycles;
+    int         left_cycles;
+    int         dump_cycles;
+    byte        *memory[MEM_SIZE];
+    program_t   **programs;
+    cursor_t    **cursors;
+} game_t;
+
+typedef struct instruction_s
+{
+    byte        opcode;
+    byte        nb_args;
+    byte        cost;
+    void        (*foo)(program_t *programs[], byte *memory[], cursor_t *cursor);
+} instruction_t;
+
+#endif
