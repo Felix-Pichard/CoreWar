4c3cc696c2b4b0f3ea7e5ffeb8f8e59f107c1ff6
Felix Pichard <felix.pichard@gmail.com>
2017-06-08 09:25:10 +0200

    Fix a bug

--- libmy/src/my_strnncpy.c
+++ libmy/src/my_strnncpy.c
@@ -17,7 +17,7 @@ char *my_strnncpy(char *src, int i, int size)
     int count;
 
     if((str = malloc(sizeof(char) * size)) == NULL)
-        my_error(5);
+        return (-1);
     for (count = 0; src[i] != '\0' && count < size; ++i, ++count)
         str[count] = src[i];
 
