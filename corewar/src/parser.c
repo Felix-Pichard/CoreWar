f2204c8ebc075cc4c8061f37aab62c21c0edefba
Felix Pichard <felix.pichard@gmail.com>
2017-06-08 10:36:31 +0200

    Handle the case where there is no args

--- corewar/src/parser.c
+++ corewar/src/parser.c
@@ -80,10 +80,10 @@ t_meta* parser(int argc, char** argv)
     int count;
 	int i;
 
-    if (argc <= 1)
-        my_error(6);
     if ((prog_args = malloc(sizeof(t_meta))) == NULL)
 		my_error(5);
+    if (argc <= 1)
+        return (prog_args);
     count = 0;
     for (i = 1; i < argc ; ++i)
     {
