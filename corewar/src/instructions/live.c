d1285c9589b45a0f02e8b044ad0665d8e83ef92a
Maniarr <maniarr.web@gmail.com>
2017-06-10 16:31:38 +0200

    add live messag

--- corewar/src/instructions/live.c
+++ corewar/src/instructions/live.c
@@ -17,6 +17,13 @@ void live(program_t *programs[], byte *memory[], cursor_t *cursor, int nb_progra
 
     warrior_id = read_byte_to_int(memory, cursor->position + 1, DIR_SIZE);
     if (warrior_id < nb_programs && programs[warrior_id]->alive != -1)
+    {
+        my_putstr("le joueur ");
+        my_put_nbr(programs[warrior_id]->id);
+        my_putstr("(");
+        my_putstr(programs[warrior_id]->name);
+        my_putstr(") est en vie\n");
         programs[warrior_id]->alive = 1;
+    }
     cursor->position = (cursor->position + 1 + DIR_SIZE) % MEM_SIZE;
 }
