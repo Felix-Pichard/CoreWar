07564139043dad1e37fdb41ca24bb3276fb4a8b1
Felix Pichard <felix.pichard@gmail.com>
2017-06-10 20:46:40 +0200

    sti WIP

--- corewar/src/instructions/sti.c
+++ corewar/src/instructions/sti.c
@@ -14,12 +14,27 @@
 void sti(program_t *programs[], byte *memory[], cursor_t *cursor, int nb_programs)
 {
     byte type_param;
+    int value;
+    int op_1;
+    int op_2;
+    int i;
+    int dcl_size;
 
     type_param = (*memory)[cursor->position + 1];
     if (!is_type_param_valid(0x0b, type_param))
     {
-        ++(cursor->position);
+        cursor->position = (cursor->position + 1) % MEM_SIZE;
+    my_putstr(": inex \n ");
         return;
     }
+    my_put_nbr(value = get_param_value_process(memory, cursor, 1));
+    my_put_nbr(op_1 = get_param_value_process(memory, cursor, 2));
+    my_put_nbr(op_2 = get_param_value_process(memory, cursor, 3));
+    my_put_nbr(op_2 + op_1);
+    my_putstr(": index \n ");
+        for (i = 0; i < 4; i++) 
+            (*memory)[op_2 + op_1] = (byte) value >> ((MAX_ARGS_NUMBER - i - 1) * 8);
+    dcl_size = get_size_param(type_param, 1) + get_size_param(type_param, 2) + get_size_param(type_param, 3);
+    cursor->position = (cursor->position + dcl_size + 2) % MEM_SIZE;
     bypass_programs(programs, nb_programs);
 }
