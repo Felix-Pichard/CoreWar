7e3c410ca36f30ef61b033305becb1cb11f27902
Felix Pichard <felix.pichard@gmail.com>
2017-06-12 09:32:02 +0200

    df

--- champion/r4v463.s
+++ champion/r4v463.s
@@ -3,10 +3,9 @@
 sti r1,%:live,%1
 live %0
 
-live:
-    live %0
-    ld r2,10
-    sti r1,r2,10
-    add r2,10,r2
-    xor r1,r1,r3
-    zjmp %:live
+live: live %0
+ld r2,10
+sti r1,r2,10
+add r2,10,r2
+xor r1,r1,r3
+zjmp %:live
